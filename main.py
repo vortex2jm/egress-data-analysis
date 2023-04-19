db2ac52c5133926ce730fb4067e8de4cf0d9d0f4
vortex2jm <joaopaulomoura5328@gmail.com>
2023-04-19 10:43:49 -0300

    :sparkles: Adding egress per courses graph

--- main.py
+++ main.py
@@ -1,15 +1,82 @@
 import pandas as pd
 import plotly.express as px
+from dash import Dash, dcc, html
+import plotly.graph_objects as go
 
+# ================================================ #
 df = pd.read_csv("egressos.csv")
-# print(df)
+courses_column = list(df['Curso UFES 2023/1'])
+courses_column.sort()
+courses_list = []
 
-edition_column = df["Edicao"]
-# edition_column = list(df["Edicao"])
-# edition_column.sort()
-# print(edition_column)
+for course in courses_column:
+    if course not in courses_list:
+        courses_list.append(course)
 
-pie_graph = px.pie(edition_column, color_discrete_sequence=px.colors.sequential.RdBu)
+students_amount_per_course = [0]*len(courses_list)
 
+for i,list_course in enumerate(courses_list):
+    for column_course in courses_column:
+        if list_course == column_course:
+            students_amount_per_course[i]+=1
 
-pie_graph.show()
+# ================================================ #
+year_graph = px.pie(df, values='Edicao', names='Edicao' , color_discrete_sequence=px.colors.sequential.RdBu)
+course_graph = go.Figure(data=[go.Pie(labels= courses_list, values=students_amount_per_course)])
+
+# Instanciating server #
+app = Dash(__name__)
+app.title = 'Introcomp'
+
+colors = {
+    'background': '#020220',
+    'text': '#FFFFFF'
+}
+# ==========================================#
+# Editando as cores dos gráficos
+year_graph.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+course_graph.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+# Criando dashboard web============================================================== #
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(children='Introcomp - Análise de egressos',
+    style={
+      'textAlign': 'center',
+      'color': colors['text']
+    }),
+    html.H3(children='''
+        Egressos por edição
+    ''',
+    style={
+      'textAlign': 'center',
+      'color': colors['text']
+    }),
+    dcc.Graph(
+        id='yg',
+        figure=year_graph
+    ),
+    html.H3(children='''
+        Egressos por curso
+    ''',
+    style={
+      'textAlign': 'center',
+      'color': colors['text']
+    }),
+    dcc.Graph(
+        id='cg',
+        figure=course_graph
+    ),
+])
+
+if __name__ == '__main__':
+    app.run_server()
+    # print(df)
+    # print("oi")
